cf3674982f9916ec3657a40bf0a144f639c0a2a6
kylind <dai.kylin@gmail.com>
2016-05-19 17:10:09 +0800

    sandbox

--- js/koa/app.js
+++ js/koa/app.js
@@ -11,6 +11,7 @@
         //执行中间件
         listen: function() {
             this._run();
+            //this.mockRun();
             console.log("run end" + this.body);
 
         },
@@ -35,8 +36,74 @@
                 yield prev;
                 console.log('xxx' + ctx.body);
             });
+        },
+        mockRun: function(){
+            var ctx = this;
+            var middlewares = ctx.middlewares;
+            var async = function*() {
+                var prev = null;
+                var i = middlewares.length;
+                while (i--) {
+                    prev = middlewares[i].call(ctx, prev);
+                }
+
+
+
+                yield prev;
+                console.log('xxx' + ctx.body);
+            };
+
+           /* var gen = async();
+
+            var gen1 = gen.next().value;
+
+            var gen2 = gen1.next().value;
+
+            var gen3 = gen2.next().value;
+
+            gen3.next();
+
+            gen2.next();
+            gen1.next();
+
+            gen.next();*/
+
+            function myRun(async){
+                var gen = async();
+
+                var isDone=false;
+
+                var middle = [];
+
+                while(!isDone){
+
+                    middle.push(gen);
+
+                    var res = gen.next();
+
+                    gen = res.value;
+
+                    isDone = res.done;
+
+                }
+
+                middle.forEach(function(item){
+
+                    item.next();
+
+
+                })
+
+
+            }
+
+            myRun(async);
+
         }
     };
+
+
+
     var app = new SimpleKoa();
     app.use(function*(next) {
 
