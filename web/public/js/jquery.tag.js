a7f9255470c749fa398830c358a889502e65c4e4
kylind <dai.kylin@gmail.com>
2017-02-13 16:34:05 +0800

    ss

--- web/public/js/jquery.tag.js
+++ web/public/js/jquery.tag.js
@@ -11,18 +11,112 @@
         init: function(options) {
 
             var defaults = {
-                inputClass: '.input-tag'
+                inputClass: '.hidden-tag',
+                tags: ['美之屋', 'BB', '韩美']
 
             };
 
             var settings = $.extend({}, defaults, options);
 
+            function getTags() {
+                return settings.tags;
+            }
+
+            $(document).bind('click', function() {
+
+
+
+
+                $('.ol-tags').each(function() {
+
+                    if ($(this).prev('.tag-input').hasClass('isActive'));
+
+                    else {
+                        $(this).remove();
+                    }
+
+
+
+                });
+
+
+
+
+
+
+
+            });
+
             return this.each(function() {
 
                 var $this = $(this);
+                var tagVal = $this.val();
+
+                var $tag = $(`<div class="tag"><span class="tag-label">${tagVal}</span><div class="tag-inputbox"><input class="tag-input" value="${tagVal}"></span></div></div>`);
 
 
+                var $label = $tag.find('.tag-label')
+                var $inputbox = $tag.find('.tag-inputbox')
+                var $input = $tag.find('.tag-input');
 
+                $label.bind('click', function() {
+                    $label.hide();
+                    $inputbox.show();
+                    $input.focus();
+                });
+
+                $input.bind('focus', function() {
+                    $('.ol-tags').remove();
+
+                    $(this).addClass('isActive');
+
+                    var tags = getTags();
+
+                    var olHtml = '<ol class="ol-tags">'
+                    tags.forEach(function(item) {
+
+                        olHtml += `<li>${item}</li>`;
+
+                    });
+                    olHtml += '</ol>';
+
+                    var $ol = $(olHtml);
+
+                    $ol.find('li').on('click', function() {
+                        $input.val($(this).text());
+                        $label.text($(this).text());
+
+                        if ($input.val() != '') {
+
+                            $inputbox.hide();
+
+                            $label.show();
+                        }
+
+                        $input.next('.ol-tags').remove();
+
+                    });
+
+                    $(this).after($ol);
+
+                });
+
+                $input.bind('blur', function() {
+
+                    $(this).removeClass('isActive');
+
+                });
+
+
+                $this.after($tag);
+
+                if (tagVal != '') {
+                    //$this.next('.tag-label').show();
+                    $label.show();
+
+                } else {
+                    $inputbox.show();
+                }
 
             });
 
@@ -48,6 +142,4 @@
 })(jQuery || avajQuery);
 
 
-
-
-
+$('.hidden-tag').tag();
