742fa320e2bb2339631dd528fe546b13d4013e28
kylind <dai.kylin@gmail.com>
2017-05-04 17:17:45 +0800

    sss

---
+++ easygoing/web/data_access/client.js
@@ -0,0 +1,55 @@
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var ObjectID = require('mongodb').ObjectID;
+var url = 'mongodb://website:zombie.123@127.0.0.1:27017/orders';//mongodb://website:zombie.123@127.0.0.1:27017/orders
+
+//mongodb://website:zombie.123@127.0.0.1:27017/orders
+
+var collection = {
+
+    queryClients: function*(filter) {
+
+        var db = yield MongoClient.connect(url);
+
+        var res = yield db.collection('clients').find(filter).toArray();
+
+        return res;
+
+    },
+
+    saveClients: function*(clients) {
+
+                var db = yield MongoClient.connect(url);
+
+                orders.forEach(function(doc) {
+                    db.collection(_name).save(doc);
+                });
+
+
+
+                return orders;
+
+    },
+
+
+    removeById: function*(id) {
+        var db = yield MongoClient.connect(url);
+
+        if (!ObjectID.isValid(id)) {
+            return { ok: 1, n: 0 };
+        } else {
+
+            var res = yield db.collection('clients').deleteOne({
+                '_id': new ObjectID(id)
+            });
+
+            return res;
+
+        }
+    }
+
+
+
+};
+
+exports.collection = collection;
