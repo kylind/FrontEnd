8cbb7bcaf5c8d5cd0b3f711bbb6b1e5e6df3d770
kylind <dai.kylin@gmail.com>
2017-05-12 17:25:35 +0800

    ss

--- easygoing/web/public/js/main-products.js
+++ easygoing/web/public/js/main-products.js
@@ -1,23 +1,18 @@
 require(['common', 'Products'], function(util, ProductsModel) {
+
     var $ = util.$;
     var ko = util.ko;
-    ko.mapping = util.mapping;
+    var mapping = util.mapping;
+    var Swiper = util.Swiper;
 
-    // var products = [
-    //     { _id: 'a', name: 'itemA', sellPrice: 8, buyPrice: 8, status: '0' },
-    //     { _id: 'b', name: 'itemB', sellPrice: 8, buyPrice: 8, status: '0' },
-    //     { _id: 'c', name: 'itemC', sellPrice: 8, buyPrice: 8, status: '0' },
-    //     { _id: 'd', name: 'itemD', sellPrice: 8, buyPrice: 8, status: '0' }
-    // ];
 
     util.initTapEvent({
-        updateAllData: null,
-        updateCurrentData: null,
-        setViewModelStatus: null,
-        swiper: null
+        updateAllData: function() {},
+        updateCurrentData: function() {},
+        setViewModelStatus: function() {},
+        swiper: {}
 
     })
-
     var productsModel = new ProductsModel(products);
 
     ko.applyBindings(productsModel, $('#products')[0]);
