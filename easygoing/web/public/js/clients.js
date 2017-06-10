7eac999c3833a6e3d2ce0823dda68520741e10c6
dai.qinglin <dai.qinglin@avature.net>
2017-06-10 18:50:22 +0800

    ss

--- easygoing/web/public/js/clients.js
+++ easygoing/web/public/js/clients.js
@@ -232,7 +232,7 @@ define(['common', 'clipboard'], function(util, Clipboard) {
                 let phone = address.phone();
                 let addressDetail = address.address();
 
-                if (recipient != '' && phone != '' && addressDetail != '') {
+                if (recipient  && phone && addressDetail) {
                     addresses += `${address.recipient()}，${address.phone()}，${address.address()}；`;
                 }
 
